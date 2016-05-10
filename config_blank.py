cf0ab3a0a4093c4de89e518e0611f3d95537a213
joertlin <jeremiah.e.oertling@gmail.com>
2016-05-09 22:53:12 -0500

    Reworked config.py file.

--- config_blank.py
+++ config_blank.py
@@ -1,3 +1,9 @@
-GIT_REPOS = ''
+ORG = ''
+
 USER = ''
 TOKEN = ''
+
+URL_BASE = r'https://api.github.com'
+URL_REPOS = '%s/orgs/%s/repos'%(URL_BASE, ORG)
+URL_TEAMS = '%s/orgs/%s/teams'%(URL_BASE, ORG)
+URL_ISSUES = '%s/orgs/%s/issues'%(URL_BASE, ORG)
