6b07e721bdd9bc752b80c3e51a2a85e5e33fba12
joertlin <jeremiah.e.oertling@gmail.com>
2016-05-29 03:20:46 -0500

    rewriting the server to not use github-flask.

--- server.py
+++ server.py
@@ -1,52 +1,44 @@
-import flask
+from flask import Flask, session, url_for, redirect, flash
+from flask import render_template_string
 import sys
 import os
-from flask.ext.github import GitHub
 
-github_client_id = os.environ.get('GITHUB_CLIENT_ID')
-github_client_secret = os.environ.get('GITHUB_CLIENT_SECRET')
+GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
+GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG')
 
-app = flask.Flask(__name__)
-app.config['GITHUB_CLIENT_ID'] = github_client_id
-app.config['GITHUB_CLIENT_SECRET'] = github_client_secret
-
-github = GitHub(app)
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(200))
-    github_access_token = Column(String(200))
-
-    def __init__(self, github_access_token):
-        self.github_access_token = github_access_token
-
-@app.before_request
-def before_request():
-    g.user = None
-    if 'user_id' in session:
-        g.user = User.query.get(session['user_id'])
+app = Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route('/')
 def index():
-    if g.user:
-        t = 'Hello! <a href="{{ url_for("user") }}">Get user</a> ' \
+    if session.get('user_id', False):
+        t = '<p>Hello! {{ session["user_id"] }}</p>'\
             '<a href="{{ url_for("logout") }}">Logout</a>'
     else:
-        #t = 'Hello! <a href="{{ url_for("login") }}">Login</a>'
-        t = 'Hello!'
+        t = 'Hello! <a href="{{ url_for("login") }}">Login</a>'
 
     return render_template_string(t)
 
-@github.access_token_getter
-def token_getter():
-    user = g.user
-    if user is not None:
-        return user.github_access_token
+@app.route('/login')
+def login():
+    if session.get('user_id', False):
+        return '%s, you are already logged in!!!'%session['user_id']
+    else:
+        session['user_id'] = 'jeremiah'
+        return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id')
+    return redirect('/')
+
+@app.route('/user')
+def user():
+    return session['user_id']
 
 @app.route('/github-callback')
-@github.authorized_handler
 def authorized(access_token):
     next_url = request.args.get('next') or url_for('index')
     if access_token is None:
@@ -61,22 +53,6 @@ def authorized(access_token):
 
     session['user_id'] = user.id
     return redirect(next_url)
-
-@app.route('/login')
-def login():
-    if session.get('user_id', None) is None:
-        return github.authorize()
-    else:
-        return 'Already logged in'
-
-@app.route('/logout')
-def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('index'))
-
-@app.route('/user')
-def user():
-    return str(github.get('user'))
 
 if __name__ == '__main__':
     app.run(debug=True)
