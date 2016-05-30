990d181d699bf77227de1218b3c05d0624e163b0
joertlin <jeremiah.e.oertling@gmail.com>
2016-05-29 21:57:13 -0500

    added git urls

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
 
+git_authorize = r'https://github.com/login/oauth/authorize'
+git_access_token = r'https://github.com/login/oauth/access_token'
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -16,6 +19,8 @@ def index():
     if session.get('user_id', False):
         t = '<p>Hello! {{ session["user_id"] }}</p>'\
             '<a href="{{ url_for("logout") }}">Logout</a>'
+        for key in session:
+            t = t + '<p>%s: %s</p>'%(key, session[key])
     else:
         t = 'Hello! <a href="{{ url_for("login") }}">Login</a>'
 
@@ -26,12 +31,12 @@ def login():
     if session.get('user_id', False):
         return '%s, you are already logged in!!!'%session['user_id']
     else:
-        session['user_id'] = 'jeremiah'
-        return redirect('/')
+        git_url = git_authorize + '?' + 'client_id='+GITHUB_CLIENT_ID + '&redirect_uri=' + url_for('authorized')
+        return redirect(git_url)
 
 @app.route('/logout')
 def logout():
-    session.pop('user_id')
+    session.clear()
     return redirect('/')
 
 @app.route('/user')
